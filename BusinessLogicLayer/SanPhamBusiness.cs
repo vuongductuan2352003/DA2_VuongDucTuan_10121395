cdad1ea4309a3f870044c14319878515dad8c2a5
vuongductuan2352003 <vuongductuan2352003@gmail.com>
2023-10-12 11:13:50 +0700

    init project

---
+++ BusinessLogicLayer/SanPhamBusiness.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using DataModel;
+using System.Reflection;
+
+namespace BusinessLogicLayer
+{
+    public class SanPhamBusiness:ISanPhamBusiness
+    {
+        private ISanPhamRepository _res;
+        public SanPhamBusiness(ISanPhamRepository res)
+        {
+            _res = res;
+        } 
+        public SanPhamModel GetChiTietSanPham(int id)
+        {
+            return _res.GetChiTietSanPham(id);
+        } 
+    }
+}
---
+++ DataModel/SanPhamModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class SanPhamModel
+    {
+        public int MaSanPham { get; set; }
+        public int MaDanhMuc { get; set; }
+        public string TenSanPham { get; set; }
+        public string AnhDaiDien { get; set; }        
+        public decimal Gia { get; set; }
+        public List<SanPhamModel> list_json_sanphamlienquan { get; set; }
+    }
+     
+}
